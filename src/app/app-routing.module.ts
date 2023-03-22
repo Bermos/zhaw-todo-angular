0c4dbc74905ecf91cc3b1dc79b31b4f1eeaee9ad
Bermos <accounts@machivenyika.ch>
2023-03-22 08:34:15 +0100

    LE05-10: Task 12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyFirstComponent } from "./my-first/my-first.component";
+import { TodoListsComponent } from "./todo-lists/todo-lists.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: TodoListsComponent
+  },
+  {
+    path: 'myfirst',
+    component: MyFirstComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
